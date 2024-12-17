cc41aafa607dbd23bef4e7299ab98de5ce7c1d7c
Nguyen Cong <ntcong95.oplai@gmail.com>
2024-12-17 09:36:32 +0700

    first commit

--- lib/dialog.dart
+++ lib/dialog.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-Future<dynamic> showYesNoDialog({required BuildContext context,
-  required String title,
-  required String content,
-  required bool isRootNavigator,
-  bool showYes = false,
-  bool showNo = false,
-  String? yesActionText,
-  String? noActionText,
-  bool barrierDismissible = false}) async {
+Future<dynamic> showYesNoDialog(
+    {required BuildContext context,
+    required String title,
+    required String content,
+    required bool isRootNavigator,
+    bool showYes = false,
+    bool showNo = false,
+    String? yesActionText,
+    String? noActionText,
+    bool barrierDismissible = false}) async {
   return showDialog(
       context: context,
       builder: (BuildContext context) {
@@ -18,62 +19,64 @@ Future<dynamic> showYesNoDialog({required BuildContext context,
           alignment: Alignment.center,
           width: double.maxFinite,
           height: double.maxFinite,
-          child: Container(
-            decoration: const BoxDecoration(
-              borderRadius: BorderRadius.all(Radius.circular(7)),
-              color: Colors.white,
-            ),
-            width: 341,
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Text(content,
-                    style: const TextStyle(
-                      color: Colors.black,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 17,
-                    )),
-                const SizedBox(
-                  height: 25,
-                ),
-                SizedBox(
-                  width: 200,
-                  child: Center(
-                      child: Row(
-                        mainAxisSize: MainAxisSize.max,
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          if (showNo)
-                            TextButton(
-                              onPressed: () {
-                                // appRouter.maybePop(false);
-                                Navigator.of(context).maybePop(false);
-                              },
-                              child: Text(
-                                noActionText ?? '',
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                ),
+          child: Scaffold(
+            body: Container(
+              decoration: const BoxDecoration(
+                borderRadius: BorderRadius.all(Radius.circular(7)),
+                color: Colors.white,
+              ),
+              width: 341,
+              padding: const EdgeInsets.all(20),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(content,
+                      style: const TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 17,
+                      )),
+                  const SizedBox(
+                    height: 25,
+                  ),
+                  SizedBox(
+                    width: 200,
+                    child: Center(
+                        child: Row(
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        if (showNo)
+                          TextButton(
+                            onPressed: () {
+                              // appRouter.maybePop(false);
+                              Navigator.of(context).maybePop(false);
+                            },
+                            child: Text(
+                              noActionText ?? '',
+                              style: const TextStyle(
+                                fontSize: 16,
                               ),
                             ),
-                          if (showYes)
-                            TextButton(
-                              onPressed: () {
-                                Navigator.of(context).maybePop(true);
-                              },
-                              child: Text(
-                                yesActionText ?? '',
-                                style: const TextStyle(
-                                  fontSize: 16,
-                                ),
+                          ),
+                        if (showYes)
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).maybePop(true);
+                            },
+                            child: Text(
+                              yesActionText ?? '',
+                              style: const TextStyle(
+                                fontSize: 16,
                               ),
-                            )
-                        ],
-                      )),
-                )
-              ],
+                            ),
+                          )
+                      ],
+                    )),
+                  )
+                ],
+              ),
             ),
           ),
         );
